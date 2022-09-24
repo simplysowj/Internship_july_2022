ad1665a27e97212578818744aeccafbdd66e423d
sowjanya <90475451+simplysowj@users.noreply.github.com>
2022-09-24 16:59:18 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-from flask import Flask,render_template,url_for,request
-import re
+import streamlit as st
+import numpy as np
+from pickle import load
+import pandas as pd
 
-app=Flask(__name__)
+from About import About_diamond
+#from EDA import show_explore_page
+import base64
+from prediction import show_predict_page
+from About import About_diamond
+from EDA import show_explore_page
+page=st.sidebar.selectbox("Explore or predict or About",{"predict","Explore","About"})
 
-@app.route('/',methods=['GET','POST'])
-def home():
-    if request.method=='POST':
-        text=request.form.get('in_1')
-        regex=request.form.get('in_2')
-        
-        pattern=re.compile(regex)
-        valid_pattern=re.findall(pattern,text)
-        numofpatterns=len(valid_pattern)
-        return render_template('index.html',valid_pattern=valid_pattern,text=text,regex=regex,number=numofpatterns)
-        print(text)
-    return render_template('index.html')
+if(page=="predict"):
 
-if __name__=="__main__":
-    app.run(debug=True)
+       
+
+    show_predict_page()
+elif(page=="Explore"):
+    show_explore_page()
+
+else:
+    
+    About_diamond()
